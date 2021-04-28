25a18153b2ccea4812c705f8fc86b9013284809f
Y-B Park <201921055@sangmyung.kr>
2021-04-28 11:45:26 +0900

    BMI by Java & Kotlin

--- app/src/main/java/com/androidapp/hellokotlin/BmiJavaActivity.java
+++ app/src/main/java/com/androidapp/hellokotlin/BmiJavaActivity.java
@@ -3,12 +3,38 @@ package com.androidapp.hellokotlin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BmiJavaActivity extends AppCompatActivity {
+    EditText heightField, weightField;
+    TextView txtResult;
+    Button btnBmi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_view_binding);
+
+        heightField = findViewById(R.id.heightField);
+        weightField = findViewById(R.id.wieghtField);
+        txtResult = findViewById(R.id.txtResult);
+        btnBmi = findViewById(R.id.btnBmi);
+
+        btnBmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sHeight = heightField.getText().toString();
+                String sWeight = weightField.getText().toString();
+                try {
+                    double bmi = Double.parseDouble(sWeight) / Math.pow(Double.parseDouble(sHeight) / 100.0, 2.0);
+                    txtResult.setText("Your BMI = " + bmi);
+                } catch (Exception e) {
+                txtResult.setText("Input(s) missing");
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/androidapp/hellokotlin/BmiKotlinActivity.kt
+++ app/src/main/java/com/androidapp/hellokotlin/BmiKotlinActivity.kt
@@ -2,10 +2,33 @@ package com.androidapp.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.lang.Exception
 
 class BmiKotlinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_view_binding)
+
+        val heightField = findViewById<EditText>(R.id.heightField)
+        val weightField = findViewById<EditText>(R.id.wieghtField)
+        val btnBmi = findViewById<Button>(R.id.btnBmi)
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+
+        btnBmi.setOnClickListener {
+//      val height = heightField.getText().toString().toDouble()
+//      val wieght = weightField.getText().toString().toDouble()
+            val sWeight = weightField.getText().toString().toDouble()
+            val sHeight = weightField.getText().toString().toDouble()
+            
+            try {
+                val bmi = sWeight.toDouble() / Math.pow(sHeight.toDouble() / 100.0, 2.0)
+                txtResult.setText("Your BMI = " + bmi)
+            } catch (e: Exception) {
+            txtResult.setText("Input(s) missing")
+            }
+        }
     }
 }

81a642a1c4f20a88464700e4952e108c2e63ca60
Y-B Park <201921055@sangmyung.kr>
2021-05-06 14:32:49 +0900

    Flow Control in Java

---
+++ app/src/main/java/com/androidapp/hellokotlin/FlowControlKotlinActivity.kt
@@ -0,0 +1,11 @@
+package com.androidapp.hellokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlowControlKotlinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_flowcontrol)
+    }
+}
--- app/src/main/java/com/androidapp/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/androidapp/hellokotlin/MainActivity.kt
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         val btnKotlin = findViewById<Button>(R.id.btnKotlin)
         val btnVarJava = findViewById<Button>(R.id.btnVarJava)
         val btnVarKotlin = findViewById<Button>(R.id.btnVarKotlin)
+        val btnFlowControlJava = findViewById<Button>(R.id.btnFlowControlJava)
+        val btnFlowControlKotlin = findViewById<Button>(R.id.btnFlowControlKotlin)
 
         btnJava.setOnClickListener {
             startActivity(Intent(this@MainActivity, BmiJavaActivity::class.java))
@@ -28,6 +30,12 @@ class MainActivity : AppCompatActivity() {
         }
         btnVarKotlin.setOnClickListener {
             startActivity(Intent(this@MainActivity, VariableKotlinActivity::class.java))
+        }
+        btnVarJava.setOnClickListener {
+            startActivity(Intent(this@MainActivity, FlowControlJavaActivity::class.java))
+        }
+        btnVarKotlin.setOnClickListener {
+            startActivity(Intent(this@MainActivity, FlowControlKotlinActivity::class.java))
         }
     }
 }
